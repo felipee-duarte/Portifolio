97b902619852a6773826b71c8af225dd3f379d05
Felipe Duarte Silva <felipeds0211@gmail.com>
2025-07-25 14:46:53 -0300

    ajustes gerais

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function clickMenu() {
-    if (paginas.style.display == 'block') {
-        paginas.style.display = 'none'
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
     } else {
-        paginas.style.display = 'block'
+        itens.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else{
+        itens.style.display = 'none'
     }
 }
