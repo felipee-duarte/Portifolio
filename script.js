5cf0d534d239ff4e0371ac1db0453012949d9857
Felipe Duarte Silva <felipeds0211@gmail.com>
2025-07-28 14:56:51 -0300

    Adicionei algumas sections

--- script.js
+++ script.js
@@ -14,3 +14,39 @@ function mudouTamanho() {
         itens.style.display = 'none'
     }
 }
+
+const swiper = new Swiper('.embalagem', {
+  loop: true,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    768: {
+        slidesPerView:2,
+    },
+    992: {
+        slidesPerView:3,
+    }
+  }
+});
