f853b466d0feeec20e766731351c9d2eef286d25
Felipe Duarte Silva <felipeds0211@gmail.com>
2025-12-01 18:18:46 -0300

    Ajustes do nodemailer

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,43 @@
-const express = require("express");
-const cors = require("cors");
-const sendEmail = require("./sendEmail");
-require("dotenv").config();
+import express from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-app.post("/enviar-formulario", sendEmail);
+app.post("/send", async (req, res) => {
+    const { nome, email, mensagem } = req.body;
 
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_USER,
+            subject: `Contato de ${nome}`,
+            html: `
+        <p><strong>Nome:</strong> ${nome}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mensagem:</strong> ${mensagem}</p>
+      `,
+        });
+
+        return res.json({ success: true });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false });
+    }
+});
+
+app.listen(process.env.PORT || 3000, () =>
+    console.log("Server rodando")
+);
